76d71873b0b773e80a5202f4a25fc4dd156f2ca0
randomguy70 <onlychessfreeze0515@gmail.com>
2021-09-17 20:37:05 -0500

    updated

--- src/main.c
+++ src/main.c
@@ -15,6 +15,9 @@
 
 enum task {NONE, CONVERT_FILE, };
 
+int convertFile(const char *fileName, const char *appvarName);
+unsigned int appvar_checksum(uint8_t *arr, size_t size);
+
 int main(int argc, char *argv[]) {
 	/* arg loop counter */
 	size_t i = 0;
@@ -22,11 +25,28 @@ int main(int argc, char *argv[]) {
 	enum task task;
 	bool convertedFile = false;
 	
+	FILE *fptr;
+	char fileName[100]= {0};
+	struct appvar appvar;
+	char appvarName[9] = {0};
+	
 	for(i = 1; i<argc; i++)
 	{
 		if(argv[i][0] == '-' && argv[i][1] == 'n')
 		{
+			if(strlen(argv[2]) > 100)
+			{
+				printf("file names must be shorter than 101 characters\nAborting..\n");
+				return 0;
+			}
+			
 			task = CONVERT_FILE;
+			strcpy(fileName, argv[2]);
+			for(uint8_t ii=0; ii<strlen(argv[2]) && ii<9; ii++)
+			{
+				appvarName[ii] = argv[2][ii];
+			}
+			
 			break;
 		}
 		task = NONE;
@@ -36,26 +56,57 @@ int main(int argc, char *argv[]) {
 	{
 		printf("arg error: ", argv[1], "\n");
 		printf(argString);
+		printf("Aborting...\n");
 		return 0;
 	}
+	
+	if(task == CONVERT_FILE)
+	{
+		
+	}
+	
 	
 	
 	return 0;
 }
 
-/*
-void createFile(const char *name)
+int writeAppvar(struct appvar *appvar, FILE *fptr)
 {
-	FILE *filePointer = fopen(name, "w+");
-	int checkSum;
-	uint16_t shortCheckSum;
+	int writePos = 0;
 	
-	// we only need the 16 least significant bytes of the int
-	shortCheckSum = (uint16_t) checkSum;
-	fwrite(&shortCheckSum, 2, 1, filePointer);
+	uint8_t output[APPVAR_MAX_FILE_SIZE];
+	size_t outputSize;
+	static const uint8_t header[APPVAR_HEADER_SIZE] = {0x2A,0x2A,0x54,0x49,0x38,0x33,0x46,0x2A,0x1A,0x0A,0x00};
+	const char *comment = "Outputted by randomguy's file converter  ";
+	uint16_t dataLen = appvar->data_len;
+	uint16_t checkSum;
 	
-	fclose(filePointer);
 	
-	return;
+	for(int i=0; i<APPVAR_HEADER_SIZE; i++)
+	{
+		output[i+writePos] = header[i];
+	}
+	writePos+=APPVAR_HEADER_SIZE;
+	
+	for(int i=0; i<APPVAR_COMMENT_SIZE; i++)
+	{
+		output[i+writePos] = comment[i];
+	}
+	writePos += APPVAR_COMMENT_SIZE;
+	
+	
 }
-*/
+
+unsigned int appvar_checksum(uint8_t *arr, size_t size)
+{
+    uint16_t checksum = 0;
+    size_t i;
+	
+    for (i = 0; i < size; ++i)
+    {
+        checksum += arr[APPVAR_VAR_HEADER_POS + i];
+        checksum &= 0xffff;
+    }
+
+    return checksum;
+}
--- src/main.h
+++ src/main.h
@@ -1,6 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdint.h>
+
 #define argString "Args:\n-n <path_to_file>\n"
+
+#define APPVAR_VAR_HEADER_POS 0x37
+#define APPVAR_HEADER_SIZE 11
+#define APPVAR_COMMENT_SIZE 42
+#define APPVAR_MAX_FILE_SIZE (64 * 1024 + 300)
+
+struct appvar {
+	char name[9];
+	int size;
+	uint8_t *data;
+	int data_len;
+	char comment[11];
+	uint16_t checkSum;
+};
 
 #endif
